f296b0292e6e4d1958c538dc585cc280c133eb01
Miracle <54377927+miraclehollins08@users.noreply.github.com>
2020-04-03 13:18:45 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var diet = {
+    name: "Apple",
+    cost: 1.99,
+    color: "red",
+    ishealthy: true
+}
+console.log(diet)
+
+var diet = {
+    name: "grapes",
+    cost: 2.99,
+    color: "purple",
+    ishealthy: true
+}
+console.log(diet)
+
+var diet = {
+    name: "soda",
+    cost: 1.99,
+    color: "orange",
+    ishealthy: false
+}
+console.log(diet)
